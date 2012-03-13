32d67c20c9e52331e00737b04ebcc14d00067082
Hiroki Inagaki <hiroki@inagaki.in>
2012-03-14 01:08:26 +0900

    Add page manupulation

--- legacybbs/bbs.go
+++ legacybbs/bbs.go
@@ -8,6 +8,8 @@ import (
 	"template"
 	"time"
 	"strings"
+    "strconv"
+    "math"
 )
 
 type Entry struct {
@@ -19,12 +21,33 @@ type Entry struct {
 	Ip string
 }
 
+type View struct {
+    Entries []Entry_view
+    Pages []Page
+    PreviousPage int
+    NextPage int
+    IsFirstPage bool
+    IsLastPage bool
+    IsError bool
+    Errors Validation
+}
+
 type Entry_view struct {
 	Name string
 	Email string
 	Title string
 	Message string
 	Date string
+}
+
+type Page struct {
+    PageNum int
+    IsCurrentPage bool
+}
+
+type Validation struct {
+    Name bool
+    Message bool
 }
 
 var sanitizing_char [][]string = [][]string{ { "&", "<", ">", "'", "\"" }, { "&amp;", "&lt;", "&gt;", "&#39;", "&quot;" } }
@@ -36,8 +59,21 @@ func init() {
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	c := appengine.NewContext(r)
+
+    currentPage, _ := strconv.Atoi(r.FormValue("page"))
+    if currentPage == 0 {
+        currentPage = 1
+    }
 	
-	q := datastore.NewQuery("Entry").Order("-Date")
+	qCount := datastore.NewQuery("Entry")
+	count, err := qCount.Count(c)
+	if err != nil {
+		http.Error(w, err.String(), http.StatusInternalServerError)
+	}
+
+    maxPage :=  int(math.Ceil(float64(count) / 10.0))
+
+	q := datastore.NewQuery("Entry").Order("-Date").Offset((currentPage - 1) * 10).Limit(10)
 	dataCount, err := q.Count(c)
 	if err != nil {
 		http.Error(w, err.String(), http.StatusInternalServerError)
@@ -49,19 +85,37 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	
-	entry_views := make([]Entry_view, dataCount)
+    view := new(View)
+    view.PreviousPage = currentPage - 1
+    view.NextPage = currentPage + 1
+    view.Pages = make([]Page, maxPage)
+    for i := 0; i < len(view.Pages); i++ {
+        view.Pages[i].PageNum = i + 1
+        if currentPage == i + 1 {
+            view.Pages[i].IsCurrentPage = true
+        } else {
+            view.Pages[i].IsCurrentPage = false
+        }
+    }
+    if currentPage == 1 {
+        view.IsFirstPage = true
+    }
+    if currentPage == maxPage {
+        view.IsLastPage = true
+    }
+	view.Entries = make([]Entry_view, dataCount)
 	for i, entry := range entries {
-		entry_views[i].Name = entry.Name
-		entry_views[i].Email = entry.Email
-		entry_views[i].Title = entry.Title
-		entry_views[i].Message = strings.Replace(entry.Message, "\n", "<br />", -1)
+		view.Entries[i].Name = entry.Name
+		view.Entries[i].Email = entry.Email
+		view.Entries[i].Title = entry.Title
+		view.Entries[i].Message = strings.Replace(entry.Message, "\n", "<br />", -1)
 		localTime := time.SecondsToLocalTime(int64(entry.Date) / 1000000)
-		entry_views[i].Date = fmt.Sprintf("%04d/%02d/%02d %02d:%02d:%02d", localTime.Year, localTime.Month, localTime.Day, localTime.Hour, localTime.Minute, localTime.Second)		
+		view.Entries[i].Date = fmt.Sprintf("%04d/%02d/%02d %02d:%02d:%02d", localTime.Year, localTime.Month, localTime.Day, localTime.Hour, localTime.Minute, localTime.Second)		
 	}
 	
 	var homeTemplate = template.Must(template.New("html").ParseFile("html/home.html"))
-	if err := homeTemplate.Execute(w, entry_views); err != nil {
-		http.Error(w, err.String(), http.StatusInternalServerError)
+	if err := homeTemplate.Execute(w, view); err != nil {
+		http.Error(w, "aa" + err.String(), http.StatusInternalServerError)
 		return
 	}
 }
